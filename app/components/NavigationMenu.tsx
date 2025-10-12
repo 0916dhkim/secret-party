de29512c8c9726f2e176bbefcfaabff5778d79ef
Danny Kim <0916dhkim@gmail.com>
2025-10-12 11:33:28 -0400

    Remove unused

--- app/components/NavigationMenu.tsx
+++ app/components/NavigationMenu.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link, useLocation } from "@tanstack/react-router";
 import { clsx } from "clsx";
 import { css } from "@flow-css/core/css";
